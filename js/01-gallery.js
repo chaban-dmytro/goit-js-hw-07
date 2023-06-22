da047b72e3b70765d234aca27b3b618a9a47a3f8
chaban-dmytro <dimach@ukr.net>
2023-06-22 13:15:56 +0200

    homework 1

---
+++ js/01-gallery.js
@@ -0,0 +1,60 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector( '.gallery' );
+
+const galleryItemsArray = [];
+
+galleryItems.forEach(item => {
+  const galleryItemEl = document.createElement( 'li' );
+  galleryItemEl.classList.add( 'gallery__item' );
+  galleryItemEl.innerHTML = `<a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a>`;
+  console.log( galleryItemEl );
+  galleryItemsArray.push( galleryItemEl );
+} );
+
+galleryEl.append( ...galleryItemsArray );
+
+galleryEl.addEventListener( 'click', onGalleryClick );
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  if ( event.target.className !== "gallery__image" ) {
+    return;
+  }
+  const instance = basicLightbox.create( `<div class="modal">
+    <img class="gallery__modal" src="${event.target.dataset.source}"/>
+    </div>` );
+  instance.show();
+     
+  const galleryModalEl = document.querySelector( '.basicLightbox' );
+  document.addEventListener( 'keydown', (event) => {
+    if ( event.code !== 'Escape' ) {
+      return
+    } else {
+      instance.close();
+    }
+  } );
+  galleryModalEl.addEventListener( 'click', closeModal );
+  
+  function closeModal( event ) {
+    instance.close();
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
